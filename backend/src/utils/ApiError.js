3764c4facc7f7273b7457966e56bbc6c7fd4ca81
Kalyan0101 <naskarkalyan2000@outlook.com>
2025-04-12 22:28:00 +0530

    update apiError file

--- backend/src/utils/ApiError.js
+++ backend/src/utils/ApiError.js
@@ -1,18 +1,19 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
-        statusCode,
-        message = "Something went wrong!!!",
+        statusCode, 
+        message = "Something went wrong!!!", 
         errors = []
-    ){
+    ) {
         console.log(this.data);
-        
+
         super(message);
         this.statusCode = statusCode;
-        this.data = null
+        this.data = null;
         this.message = message;
-        this.success = false
-        this.errors = errors
-    }    
+        this.success = false;
+        this.errors = errors;
+        this.stack = null;
+    }
 }
 
-export { ApiError };
+export { ApiError };    
