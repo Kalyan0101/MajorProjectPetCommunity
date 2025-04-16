f9289937a6cd9347e384121d4c8507843d51ea1b
Kalyan0101 <naskarkalyan2000@outlook.com>
2025-04-16 13:33:43 +0530

    fix spelling

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -4,7 +4,7 @@ import { ApiError } from "../utils/ApiError";
 import { User } from "../models/user.model";
 
 
-export const varifyJWT = asyncHandler(async (req, _, next) => {
+export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");    
         if(!token) throw new ApiError(401, "Unauthorized request!!!");
