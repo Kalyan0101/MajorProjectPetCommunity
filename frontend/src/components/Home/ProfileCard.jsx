86713c70b0227511570289af24d3f1a707a146a8
Kalyan0101 <naskarkalyan2000@outlook.com>
2025-05-03 22:17:39 +0530

    store related bug fixed and update code of profile

--- frontend/src/components/Home/ProfileCard.jsx
+++ frontend/src/components/Home/ProfileCard.jsx
@@ -1,39 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useSelector } from "react-redux"
 
 const ProfileCard = () => {
-  const [profile, setProfile] = useState(null); // Store profile data
-  const [loading, setLoading] = useState(true); // Loading state
+  const userData = useSelector(state => state.auth.userData);
 
-  // Fetch profile data from backend on component mount
-  // useEffect(() => {
-  //   const fetchProfile = async () => {
-  //     try {
-  //       const response = await axios.get('http://localhost:5000/api/profile'); // Replace with your API endpoint
-  //       setProfile(response.data); // Assuming the response returns the profile object
-  //     } catch (error) {
-  //       console.error("Error fetching profile data:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchProfile();
-  // }, []);
-
-  if (loading) {
-    return <p>Loading profile...</p>; // Loading indicator while data is being fetched
-  }
-
-  return (
+  return userData && (
     <div className="bg-white shadow p-4 rounded-md">
       <img
-        src={profile?.profileImage || "https://via.placeholder.com/150"} // Default image if profileImage is not available
+        src={ userData?.avatar.url }
         alt="profile"
-        className="w-24 h-24 rounded-full mx-auto"
+        className="w-24 h-24 rounded-full mx-auto object-cover object-center"
       />
-      <h2 className="text-center font-semibold mt-2">{profile?.ownerName || "Pet Owner Name"}</h2>
-      <p className="text-center text-gray-500 text-sm">{`Pet Name: ${profile?.petName || "Fluffy"}`}</p>
+      <h2 className="text-center font-semibold mt-2">{ userData?.fullName }</h2>
+      <p className="text-center text-gray-500 text-sm">{`Pet Name: ${ "Fluffy"}`}</p>
     </div>
   );
 };
